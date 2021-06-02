dd02d1146ef964303e5734b9784a9205e9ea2a64
danielcollione <daniel.collione22@gmail.com>
2021-06-02 18:09:31 -0300

    commit

--- websocket-server.js
+++ websocket-server.js
@@ -1,19 +1,39 @@
+// var fs = require('file-system');
+
+// var privateKey = fs.readFileSync("./cert/key.pem", "utf8");
+// var certificate = fs.readFileSync("./cert/cert.pem", "utf8");
+
+// var credentials = { key: privateKey, cert: certificate };
+
+// const http = require('https');
+// const WebSocketServer = require('websocket').server;
+
+// const server = http.createServer();
+// server.listen(8081);
+
+// const wss = new WebSocketServer({
+//     httpServer: server
+// });
+
+// wss.on("connection", function connection(ws) {
+//   ws.on("message", function incoming(message) {
+//     console.log("received: %s", message);
+//   });
+
+//   ws.send("something");
+// });
+const https = require("https");
+const WebSocket = require("ws");
+
 var fs = require('file-system');
 
-var privateKey = fs.readFileSync("./cert/key.pem", "utf8");
-var certificate = fs.readFileSync("./cert/cert.pem", "utf8");
+var privateKey = fs.readFileSync("./cert/server.key", "utf8");
+var certificate = fs.readFileSync("./cert/server.cert", "utf8");
 
 var credentials = { key: privateKey, cert: certificate };
 
-const https = require('https');
-const WebSocketServer = require('websocket').server;
-
 const server = https.createServer(credentials);
-server.listen(8081);
-
-const wss = new WebSocketServer({
-    httpServer: server
-});
+const wss = new WebSocket.Server({ server });
 
 wss.on("connection", function connection(ws) {
   ws.on("message", function incoming(message) {
@@ -22,3 +42,5 @@ wss.on("connection", function connection(ws) {
 
   ws.send("something");
 });
+
+server.listen(8081);
